2e2a6c9316e7ce1969f0bb7c45222a2a70bcbc73
MinhTai940 <duongtam123109@gmail.com>
2025-10-15 16:29:16 +0700

    Delete backend directory

--- backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-// backend/routes/user.js
-const router = require("express").Router();
-const userCtrl = require("../controllers/userController");
-
-router.get("/", userCtrl.list);    // GET /users
-router.post("/", userCtrl.create); // POST /users
-
-module.exports = router;
